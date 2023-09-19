5af5755c96ef18a41deb46268508f9ebb7517807
JocelynVelarde <a01285780@itesm.mx>
2023-09-19 12:15:53 -0600

    modified model for flask

--- hello.py
+++ hello.py
@@ -1,31 +1,44 @@
 from flask import Flask, render_template, request
-
-# Import your Chatbot class and other dependencies here
 from revChatGPT.V3 import Chatbot
 
-# Create a Flask web application
 app = Flask(__name__)
 
-# Create a chatbot with your API key
-API_KEY = "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"
+API_KEY = "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"
 chatbot = Chatbot(API_KEY)
 
-# Initialize an empty conversation list
 conversation = []
 
-# Define a route to display the chat interface
 @app.route("/", methods=["GET", "POST"])
 def chat():
     if request.method == "POST":
-        user_message = request.form["user_message"]
-        # Add the user's message to the conversation
+        source_prompt = request.form.get("source_prompt")
+        user_message = ""
+
+        if "file" in request.files:
+            uploaded_file = request.files["file"]
+            if uploaded_file.filename != "":
+                # Read the content of the uploaded file
+                user_message = uploaded_file.read().decode("utf-8")
+        else:
+            user_message = request.form["user_message"]
+        
+        if source_prompt == "1":
+            emotion_prompt = "Analiza las palabras que se utilizan en la entrevista y asignales una emoción. Entrega un párrafo con las distintas emociones que encontraste en el tono de la entrevista y muéstrame su descripción."
+            response = chatbot.ask(emotion_prompt)
+        elif source_prompt == "2":
+            keywords_prompt = "Explica en un párrafo las palabras clave que le dan contexto a la entrevista, son palabras útiles que usa el vendedor para convencer al cliente."
+            response = chatbot.ask(keywords_prompt)
+        elif source_prompt == "3":
+            tone_prompt = "Analiza el tono de la entrevista y entrega un párrafo con la descripción de este."
+            response = chatbot.ask(tone_prompt)
+        elif source_prompt == "4":
+            meta_analysis_prompt = "Realiza un meta-análisis de la estructura y el flujo de la entrevista en un párrafo, es decir, la introducción y conceptos clave, el cuerpo de la entrevista y la conclusión. Indica en qué sección se mostró un mayor interés del cliente."
+            response = chatbot.ask(meta_analysis_prompt)
+        else:
+            response = "Option not recognized."
+
         conversation.append(("User: " + user_message, "user"))
-
-        # Send the message to your chatbot logic and get the response
-        response = chatbot.ask(user_message)
-
-        # Add the chatbot's response to the conversation
-        conversation.append(("Chatbot: " + response, "chatbot"))
+        conversation.append(("Chatbot: " + response, "Chatbot"))
 
     else:
         response = "Ask me something."
@@ -34,3 +47,35 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
