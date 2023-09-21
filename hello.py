f043c83792da35a68cdb75d8fde89e1f066893f6
JocelynVelarde <a01285780@itesm.mx>
2023-09-21 07:20:39 -0600

    changed prompts

--- hello.py
+++ hello.py
@@ -27,16 +27,16 @@ def chat():
             user_message = request.form["user_message"]
         
         if source_prompt == "1":
-            emotion_prompt = "Analiza las palabras que se utilizan en la entrevista y asignales una emoción. Entrega un párrafo con las distintas emociones que encontraste en el tono de la entrevista y muéstrame su descripción."
+            emotion_prompt = "Analiza esta llamada telefónica enfocada a agendar una cita para que el cliente conozca un proyecto inmobiliario y describe las emociones del vendedor y comprador en un párrafo para cada uno. Trata de identificar si alguna de estas emociones ayudó a que se concretara una cita en caso de que se hubiera agendado, o qué emociones pudieron haber causado que no se lograra agendar una cita."
             response = chatbot.ask(emotion_prompt)
         elif source_prompt == "2":
-            keywords_prompt = "Explica en un párrafo las palabras clave que le dan contexto a la entrevista, son palabras útiles que usa el vendedor para convencer al cliente."
+            keywords_prompt = "Lista las palabras clave en esta llamada telefónica enfocada a agendar una cita para que el cliente conozca un proyecto inmobiliario, enfócate en las palabras clave que usaron el vendedor y el comprador que consideras que ayudaron a que se concretara una cita en caso de que se hubiera agendado, o qué palabras clave pudieron haber causado que no se lograra agendar una cita."
             response = chatbot.ask(keywords_prompt)
         elif source_prompt == "3":
-            tone_prompt = "Analiza el tono de la entrevista y entrega un párrafo con la descripción de este."
+            tone_prompt = "Analiza esta llamada telefónica enfocada a agendar una cita para que el cliente conozca un proyecto inmobiliario, e identifica si el vendedor respondió las preguntas del cliente aunque no sepas si son correctas o incorrectas. En un segundo párrafo di si se tocaron estos puntos: 1. precio de los departamentos, 3. si el cliente quiere habitar el departamento o comprarlo como inversión 2. ubicación, 3. esquema de rendimientos o rentas de la construcción, 4. invitarlo a una cita."
             response = chatbot.ask(tone_prompt)
         elif source_prompt == "4":
-            meta_analysis_prompt = "Realiza un meta-análisis de la estructura y el flujo de la entrevista en un párrafo, es decir, la introducción y conceptos clave, el cuerpo de la entrevista y la conclusión. Indica en qué sección se mostró un mayor interés del cliente."
+            meta_analysis_prompt = "Realiza un meta-análisis de la estructura y el flujo de  esta llamada telefónica enfocada a agendar una cita para que el cliente conozca un proyecto inmobiliario, identifica los 3 puntos principales que consideras que ayudaron a que se concretara una cita en caso de que se hubiera agendado, y que puntos durante el flujo de la llamada pudieron haber causado que no se lograra agendar una cita. Indica en qué parte de la llamada se mostró un mayor interés del cliente."
             response = chatbot.ask(meta_analysis_prompt)
         else:
             response = "Option not recognized."
