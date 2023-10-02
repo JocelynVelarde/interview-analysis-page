0c512da129aa2cc0ce8b7736f413a2623fe225fb
JocelynVelarde <a01285780@itesm.mx>
2023-10-02 12:52:30 -0600

    allows uploading multiple files and analyze each one individually with written prompt

--- hello2.py
+++ hello2.py
@@ -4,7 +4,7 @@ from config import API_KEY
 
 app = Flask(__name__)
 
-chatbot = Chatbot("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")
+chatbot = Chatbot("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")
 
 conversation = []
 
@@ -12,30 +12,28 @@ conversation = []
 def chat():
     if request.method == "POST":
         user_prompt = request.form.get("user_prompt")
-        user_message = ""
+        uploaded_files = request.files.getlist("files")
+        #user_message = ""
 
-        if "file" in request.files:
-            uploaded_file = request.files["file"]
+        for uploaded_file in uploaded_files:
             if uploaded_file.filename != "":
-                # Read the content of the uploaded file
                 user_message = uploaded_file.read().decode("utf-8")
-        else:
-            user_message = request.form["user_message"]
 
-        # Combine the user's custom prompt and the uploaded text file for analysis
-        combined_prompt = user_prompt + " " + user_message
-        
-        # Use ChatGPT to analyze the combined prompt and text file
-        response = chatbot.ask(combined_prompt)
+                # Combine the user's custom prompt and the uploaded text file for analysis
+                combined_prompt = user_prompt + " " + user_message
+                
+                # Use ChatGPT to analyze the combined prompt and text file
+                response = chatbot.ask(combined_prompt)
 
-        conversation.append((user_prompt, "User Prompt"))
-        conversation.append((user_message, "Uploaded Text"))
-        conversation.append((response, "Response"))
+                conversation.append((user_prompt, "User Prompt"))
+                conversation.append((user_message, "Uploaded Text"))
+                conversation.append((response, "Response"))
 
     else:
         response = "Ask me something."
 
     return render_template("example22.html", conversation=conversation, response=response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
