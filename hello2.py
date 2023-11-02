afa9a4e1284e95835e499b44dd1eda9c35e44dbe
JocelynVelarde <a01285780@itesm.mx>
2023-11-02 11:24:01 -0600

    added transcription using assembly ai, responses updated in gsheets. Error management with correct messages

--- hello2.py
+++ hello2.py
@@ -1,24 +1,27 @@
 import json
-from flask import Flask, render_template, jsonify, request
+import os
+from flask import Flask, render_template, request
 from revChatGPT.V3 import Chatbot
 from auth import spreadsheet_service
-from google.cloud import speech
-from google.oauth2 import service_account
+import assemblyai as aai
+
+
 
 app = Flask(__name__)
 
-chatbot = Chatbot("key")
+chatbot = Chatbot("")
 
-spreadsheet_id = '1EyFOt8CQmxNi9lKRLWCnHIYUCf-mDjLFX6jrFjTPUO0' 
+spreadsheet_id = '1EyFOt8CQmxNi9lKRLWCnHIYUCf-mDjLFX6jrFjTPUO0'
 
 conversation = []
+
 
 @app.route("/", methods=["GET", "POST"])
 def chat():
     if request.method == "POST":
         user_prompt = request.form.get("user_prompt")
         uploaded_files = request.files.getlist("files")
-        #user_message = ""
+        # user_message = ""
 
         for uploaded_file in uploaded_files:
             if uploaded_file.filename != "":
@@ -26,10 +29,10 @@ def chat():
 
                 # Combine the user's custom prompt and the uploaded text file for analysis
                 combined_prompt = user_prompt + " " + user_message
-                
+
                 # Use ChatGPT to analyze the combined prompt and text file
                 response = chatbot.ask(combined_prompt)
-                
+
                 conversation.append((user_prompt, "User Prompt"))
                 conversation.append((user_message, "Uploaded Text"))
                 conversation.append((response, "Response"))
@@ -41,77 +44,87 @@ def chat():
 
     return render_template("example22.html", conversation=conversation, response=response)
 
+
 def write_data_to_sheet(texto):
     data = json.loads(texto)
-    range_name = 'Sheet1!A2:R'  # Modifica esto para que abarque toda la columna donde deseas agregar datos.
-    
+    # Modifica esto para que abarque toda la columna donde deseas agregar datos.
+    range_name = 'Sheet1!A2:R'
+
     # Obten los datos actuales en la hoja de cálculo.
-    result = spreadsheet_service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
-    
+    result = spreadsheet_service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=range_name).execute()
+
     values = result.get('values', [])
-    
+
     if not values:
         # Si no hay datos en la hoja de cálculo, agrega las llaves como la primera fila.
         values.append(list(data.keys()))
-    
+
     # Agrega los nuevos datos al final de la lista.
     values.append(list(data.values()))
-    
+
     # Actualiza la hoja de cálculo con los datos combinados.
     body = {
         'values': values
     }
-    
+
     result = spreadsheet_service.spreadsheets().values().update(
         spreadsheetId=spreadsheet_id, range=range_name,
         valueInputOption='USER_ENTERED', body=body).execute()
-    
+
     print('{0} cells updated.'.format(result.get('updatedCells')))
     print(result)
 
 
-@app.route('/transcribe', methods=['GET'])
-
+@app.route('/transcribe', methods=['GET', 'POST'])
 def do_transcription():
-    print("transcription route")
-    gcs_uri = "gs://audiofile01/audioTwo.wav"
-    transcription = transcribe_gcs(gcs_uri)
-    return transcription
+    # Replace with your API token
+    
+    transcript = ""  # Initialize transcript variable
+    
+    if request.method == 'POST':
+        if 'audio_file' in request.files:
+                audio_file = request.files['audio_file']
+                file_path = os.path.join(os.getcwd(), audio_file.filename)
+                audio_file.save(file_path)
+                print(file_path)
 
-# Function to transcribe audio
-def transcribe_gcs(gcs_uri):
-    print("transcribe_gcs function")
-    # Your transcription code here
-    client_file = 'sa_speech_text.json'
-    credentials = service_account.Credentials.from_service_account_file(client_file)
-    client = speech.SpeechClient(credentials=credentials)
+                config = aai.TranscriptionConfig(language_code="es", speaker_labels=True)
+                transcriber = aai.Transcriber(config=config)
+                transcript = transcriber.transcribe(file_path)
+                write_transcription(transcript.text)
+                os.remove(file_path)
+        else: 
+            return render_template('error.html')
+    #transcripcion mandarla a google sheet
+    # Return the transcript
+    return render_template('success.html', transcript=transcript.text)
+   
 
-    audio = speech.RecognitionAudio(uri=gcs_uri)
-    config = speech.RecognitionConfig(
-        encoding='LINEAR16',
-        sample_rate_hertz=8000,
-        language_code="es-MX",
-        audio_channel_count=2,
-    )
+def write_transcription(texto):
+    spreadsheet_id = "1Vs-EdeZcPZ5Ozac1-_AZQ-l35rMffaZukoDlzEXhl2Y"
+    range_name = 'Transcripción!A:A'
 
-    operation = client.long_running_recognize(config=config, audio=audio)
+    result = spreadsheet_service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=range_name).execute()
+    
+    values = result.get('values', [])
+    next_row = len(values) + 1 
 
-    print("Waiting for operation to complete...")
-    response = operation.result(timeout=90)
+    body = {
+        'range': f'Transcripción!A{next_row}',  
+        'majorDimension': 'ROWS',
+        'values': [
+            [texto]  
+        ]
+    }
 
-    transcript_builder = []
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
-    for result in response.results:
-        # The first alternative is the most likely one for this portion.
-        transcript_builder.append(f"{result.alternatives[0].transcript}")
-        #transcript_builder.append(f"\nConfidence: {result.alternatives[0].confidence}")
+    result = spreadsheet_service.spreadsheets().values().update(
+        spreadsheetId=spreadsheet_id, range=f'Transcripción!A{next_row}',
+        valueInputOption='USER_ENTERED', body=body).execute()
 
-    transcript = "".join(transcript_builder)
-    print(transcript)
-
-    return transcript
-
+    print('New response written to row', next_row)
+    print(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
